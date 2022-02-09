3aca636ad32a99a0b9ad9b178208452a4c3f9bfd
Rajeswari4526 <g.rajeswari45@gmail.com>
2022-02-09 20:54:03 +0000

    Add network module 1

--- network.tf
+++ network.tf
@@ -1,5 +1,5 @@
 module "network" {
   source  = "app.terraform.io/jollycorp/network/azurerm"
   version = "3.5.0"
-  resource_group_name = "${var.prefix}-workshop"
+  resource_group_name = "rajeswari-g-workshop"
 }
